aa9290e6eff80958d7d183b67bf9f773928169f0
Xavier Henry <xavhenry@hotmail.fr>
2019-06-17 22:57:06 +0200

    Lire le fichier parcelles

--- Data.py
+++ Data.py
@@ -33,11 +33,18 @@ class Data:
 
     def ajouter_culture(ioSelf, inCulture):
         ioSelf._dictCultures[inCulture.nom()] = inCulture 
-        pass
 
     
     def culture_par_nom(inSelf, inNom):
         return inSelf._dictCultures[inNom]
+
+
+    def ajouter_parcelle(ioSelf, inParcelle):
+        ioSelf._dictParcelles[inParcelle.nom()] = inParcelle 
+
+    
+    def parcelle_par_nom(inSelf, inNom):
+        return inSelf._dictParcelles[inNom]
 
 
     def lire_fichier_cultures(ioSelf, inNomFichierCsv ):
@@ -75,34 +82,31 @@ class Data:
         return True
 
 
-    def read_exploitations(ioSelf, inNomFichierCsv): # return boolean
+    def lire_fichier_parcelles(ioSelf, inNomFichierCsv): # return boolean
         """
         ROLE : lit le fichier CSV de nom spécifié dans Data ioSelf
         ENTREE inNomFichierCsv : str # nom du fichier CSV à traiter
         SORTIE False en cas d'erreur de l'opération, True sinon
         MAJ    ioSelf : Data
         """
-        #1 Lecture du fichier dans la variable lstCultures
-        fichCultures = open(inNomFichierCsv,'r')
-        lstCultures = fichCultures.readlines() # ou lstPoints = list(fichPointsRando)
-        fichCultures.close()
-        #2 Ajout des différents points dans la trace ioSelf
+        #1 Lecture du fichier dans la variable lstParcelles
+        fichParcelles = open(inNomFichierCsv,'r')
+        lstParcelles = fichParcelles.readlines() # ou lstParcelles = list(fichParcelles)
+        fichParcelles.close()
+        #2 Ajout des différentes parcelles dans la Data ioself
         #  en sautant la ligne d'en-tête (avec le nom de colonnes)
-        for LigneCulture in lstCultures[1:] : # traiter une ligne de culture
-            lstDatas = LigneCulture.rstrip().split(";") # isoler chaque élément séparé par point-virgule
-            # Culture;Nom;Irriguation;Récolte;Semence;Durée
-            cultureType, nom, irriguation, recolte, semence, duree = lstDatas
-            irriguation = True if irriguation == 'Oui' else False # conversion de str en booléen 
-            #créer une culture en fonction de cultureType
-            if cultureType == 'Annuelle':
-                culture = Annuelle(nom, irriguation, recolte, semence)
-            elif cultureType == 'Perenne':
-                culture = Perenne(nom, irriguation, recolte, duree)
-            else:
-                print(cultureType, ': Culture ignorée')
-                continue 
+        if len(lstParcelles[0].split(";")) != 8:
+            print(inNomFichierCsv, ': Le fichier doit avoir 8 colonnes')
+            return False
 
-            ioSelf._lstCultures.append(culture)
+        for LigneParcelle in lstParcelles[1:] : # traiter une ligne de parcelle
+            lstDatas = LigneParcelle.rstrip().split(";") # isoler chaque élément séparé par point-virgule
+            # Nom;Proprietaire;Largeur;Hauteur;Centre;Irriguee;Orientation;Culture en cours
+            name, owner, width, height, center, irriguee, orientation, actualCrop = lstDatas
+            irriguee = irriguee == 'Oui' # conversion de str en booléen 
+            actualCrop = ioSelf.culture_par_nom(actualCrop) # Recherche de l'objet culture de même nom
+            parcelle = Parcelle(name, width, height, center, irriguee, orientation, owner, actualCrop)
+            ioSelf.ajouter_parcelle(parcelle)
 
         return True
 
@@ -132,6 +136,13 @@ class Culture(Nommable):
         outSelf._lstParcelles = []
         
 
+    def ajouter_parcelle(ioSelf, inParcelle):
+        """
+        ROLE mise à jour de l'association Culture-Parcelle
+        """
+        ioSelf._lstParcelles.append(inParcelle)
+        
+
 class Zone(Nommable):
     """
     ROLE gère les zones géographiques rectangles, calcule l'aire et le périmètre
@@ -145,7 +156,7 @@ class Zone(Nommable):
         MAJ ioself : Zone
         """
         super().__init__(inName)
-        outSelf._width = inWidth.
+        outSelf._width = inWidth
         outSelf._height = inHeight
         outSelf._center = inCenter
         
@@ -159,7 +170,7 @@ class Zone(Nommable):
         l = inSelf._height
         areaMeter = L*l
         areaHa = areaMeter*0.001 # conversion en mètres
-        return areaHa.
+        return areaHa
 
 
     def perimeter(inSelf):    
@@ -191,13 +202,37 @@ class Prairie(Perenne):
     
 
 class Parcelle(Zone):
-    def __init__(self):
-        self.irriguated = False
-        self.orientation = Orientation.NORTH
+    def __init__(outSelf, inName, inWidth, inHeight, inCenter, inIrriguated, inOrientation, inOwner, ioActualCrop):
+        super().__init__(inName, inWidth, inHeight, inCenter)
+        outSelf._irriguated = inIrriguated
+        outSelf._orientation = inOrientation
+        outSelf._owner = inOwner
+        # mise à jour de l'association Culture-Parcelle (bidirectionnelle)
+        outSelf._actualCrop = ioActualCrop
+        ioActualCrop.ajouter_parcelle(outSelf)
+        pass
+
+
+class Exploitation(Nommable):
+    """
+    ROLE définit un ensemble de parcelles appartenant toutes à un même propriétaire.
+    """
+    def __init__(outSelf, inName, inOwner):
+        """
+        PROCEDURE créant une exploitation vide ne contenant aucune parcelle
+        ENTREE inName : str # Nom de l'exploitation
+               inOwner :str # Nom du propriétaire
+        SORTIE outSelf : exploitation
+        """
+        super().__init__(inName) # Nom de l'exploitation
+        outSelf._Owner =str(inOwner) # Nom du propriétaire
+        outSelf._lstParcelles = list() # Liste des exploitations 
+
+    
+    def ajouter_parcelles(ioSelf):
+        pass
+
         
-        self.exploitation = None
-        self.culture = None
-        self.patch = []
         
 
 class Cereales(Annuelle):
@@ -210,4 +245,5 @@ class Oleagineux(Annuelle):
 
 if __name__ == '__main__': # Test unitaire du fichier Data.py 
     data = Data()
-    data.read_cultures('Cultures.csv') 
+    data.lire_fichier_cultures('Cultures.csv') 
+    data.lire_fichier_parcelles('Parcelles.csv')
